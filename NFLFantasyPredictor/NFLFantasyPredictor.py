1380745408a87df732d3465ad7c97abb932f1245
niiicky <85355923+niiicky@users.noreply.github.com>
2025-05-12 01:49:13 -0400

    Changed to predict total statistics of a player in a season
    - Need to update and change the models

--- NFLFantasyPredictor/NFLFantasyPredictor.py
+++ NFLFantasyPredictor/NFLFantasyPredictor.py
@@ -8,8 +8,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.preprocessing import PowerTransformer
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input
 import math
-import pylab
 import scipy.stats as stats
 from scipy.stats import norm, skew
 
@@ -25,14 +27,30 @@ cols = ['player_id', 'player_name', 'position', 'season', 'season_type',
         'receptions', 'targets', 'receiving_yards',
         'receiving_tds', 'receiving_fumbles', 'fantasy_points']
 
-df = pd.DataFrame(nfl.import_weekly_data([2020, 2021, 2022, 2023, 2024], cols))
+years = [2020, 2021, 2022, 2023, 2024]
+
+s = nfl.import_seasonal_rosters(years)
+s = s[['player_id', 'season', 'age']]
+s.dropna(axis = 0, subset=['age'], inplace=True)
+
+df = pd.DataFrame(nfl.import_weekly_data(years, cols))
 
 df = df[(df['season_type'] == 'REG') & (df['position'] == 'WR')]
 
-df_numerics = df.select_dtypes(include=np.number)
+print(df[(df['player_name'] == 'J.Jefferson') & (df['season'] == 2023)])
 
-print(df_numerics.head(20))
-print(df_numerics.info())
+df_totals = df.groupby(['player_id', 'player_name', 'season']).agg({'receptions': 'sum', 'targets': 'sum', 
+                                                       'receiving_yards': 'sum', 'receiving_tds': 'sum', 
+                                                       'receiving_fumbles': 'sum'}).reset_index()
+
+df_merged = df_totals.merge(s, on=['player_id', 'season'])
+
+df_merged['nextSeason_receiving_yards'] = df_merged.groupby(['player_id'])['receiving_yards'].shift(-1)
+
+
+print(df_merged.head())
+print(df_merged.info())
+print(df_merged[df_merged['player_name'] == 'J.Jefferson'])
 
 
 # EDA
@@ -49,6 +67,9 @@ print(df_numerics.info())
 # fig = plt.figure()
 # res = stats.probplot(df_numerics['fantasy_points'], plot=plt)
 # plt.show()
+
+
+# Preprocessing
 
 # ----------------------------------------------------------------------- #
 
@@ -76,35 +97,35 @@ print(df_numerics.info())
 
 # modelling
 
-X = df_numerics.drop(['season', 'fantasy_points'], axis = 1)
-y = df_numerics['fantasy_points']
+# X = df_merged.drop(['player_id', 'player_name', 'season'], axis = 1)
+# y = df_merged['fantasy_points']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-lr_model = LinearRegression()
-rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-gb_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
+# lr_model = LinearRegression()
+# rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+# gb_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
 
-lr_model.fit(X_train, y_train)
-rf_model.fit(X_train, y_train)
-gb_model.fit(X_train, y_train)
+# lr_model.fit(X_train, y_train)
+# rf_model.fit(X_train, y_train)
+# gb_model.fit(X_train, y_train)
 
-fantasy_points_preds = lr_model.predict(X_test)
-fantasy_points_preds2 = rf_model.predict(X_test)
-fantasy_points_preds3 = gb_model.predict(X_test)
+# fantasy_points_preds = lr_model.predict(X_test)
+# fantasy_points_preds2 = rf_model.predict(X_test)
+# fantasy_points_preds3 = gb_model.predict(X_test)
 
-mse = mean_squared_error(y_test, fantasy_points_preds)
-rmse = math.sqrt(mean_squared_error(y_test, fantasy_points_preds))
-r2 = r2_score(y_test, fantasy_points_preds)
+# mse = mean_squared_error(y_test, fantasy_points_preds)
+# rmse = math.sqrt(mean_squared_error(y_test, fantasy_points_preds))
+# r2 = r2_score(y_test, fantasy_points_preds)
 
-mse2 = mean_squared_error(y_test, fantasy_points_preds2)
-rmse2 = math.sqrt(mean_squared_error(y_test, fantasy_points_preds2))
-r2_2 = r2_score(y_test, fantasy_points_preds2)
+# mse2 = mean_squared_error(y_test, fantasy_points_preds2)
+# rmse2 = math.sqrt(mean_squared_error(y_test, fantasy_points_preds2))
+# r2_2 = r2_score(y_test, fantasy_points_preds2)
 
-mse3 = mean_squared_error(y_test, fantasy_points_preds3)
-rmse3 = math.sqrt(mean_squared_error(y_test, fantasy_points_preds3))
-r2_3 = r2_score(y_test, fantasy_points_preds3)
+# mse3 = mean_squared_error(y_test, fantasy_points_preds3)
+# rmse3 = math.sqrt(mean_squared_error(y_test, fantasy_points_preds3))
+# r2_3 = r2_score(y_test, fantasy_points_preds3)
 
-print(f"MSE: {mse:.2f} RF: {mse2:.2f} GB: {mse3:.2f}")
-print(f"RMSE: {rmse:.2f} RF: {rmse2:.2f} GB: {rmse3:.2f}")
-print(f"R2 Score: {r2:.4f} RF: {r2_2:.4f} GB: {r2_3:.4f}")
+# print(f"MSE: {mse:.2f} RF: {mse2:.2f} GB: {mse3:.2f}")
+# print(f"RMSE: {rmse:.2f} RF: {rmse2:.2f} GB: {rmse3:.2f}")
+# print(f"R2 Score: {r2:.4f} RF: {r2_2:.4f} GB: {r2_3:.4f}")
